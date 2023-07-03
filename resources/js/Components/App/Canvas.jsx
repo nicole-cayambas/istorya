e87d74ae40a23156fafc626566a266a981c010f5
Nicole Cayambas <nicolescayambas@gmail.com>
2023-07-03 23:13:11 +0800

    #5 UI initialized

--- resources/js/Components/App/Canvas.jsx
+++ resources/js/Components/App/Canvas.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Heading3 } from '../Generic/Typography'
 
-const ReadingCanvas = ({story}) => {
+const ReadingCanvas = ({ story }) => {
   return (
-    <div>{JSON.stringify(story)}</div>
+    <div className='drop-shadow-lg bg-white rounded-2xl p-8 flex flex-col gap-4 break-words'>
+      <Heading3>Chapter</Heading3>
+      <p>
+        {JSON.stringify(story)}
+      </p>
+      <p>
+        {JSON.stringify(story)}
+      </p>
+      <p>
+        {JSON.stringify(story)}
+      </p>
+      <p>
+        {JSON.stringify(story)}
+      </p>
+      <p>
+        {JSON.stringify(story)}
+      </p>
+    </div>
   )
 }
 
